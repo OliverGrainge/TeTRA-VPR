798cf1ecdd75a031817d921a1666490f73d518ef
olivergraininge <oliver@grainge.me>
2025-05-07 12:22:29 +0100

    adding code

--- test.py
+++ test.py
@@ -1,16 +1,12 @@
 from models.helper import get_model 
-
-import torch.nn as nn
 import torch 
 
+model = get_model(backbone_arch="vitbaset", agg_arch="boq", image_size=(322,322))
 
-def measure_mem(model): 
-    sd = model.state_dict()
-    mem = sum(p.numel() * p.element_size() for p in sd.values())
-    print(f"Model size: {mem / 1024 / 1024} MB")
-    return mem
-
-model = get_model(backbone_arch="vitbaset", agg_arch="boq", image_size=322, desc_divider_factor=1)
-measure_mem(model)
-model.deploy()
-measure_mem(model)
+sd = torch.load("checkpoints/TeTRA-finetune/logistic/VitbaseT322_BoQ-DescDividerFactor[1]/epoch=1-MSLS_val_q_R1=84.71.ckpt", weights_only=True, map_location="cpu")["state_dict"]
+                                                                                                                                                                
+print(list(model.state_dict().keys()))
+print("===============================" * 100)
+print(list(sd.keys()))
+print("===============================" * 100)
+model.load_state_dict(sd, strict=True)
